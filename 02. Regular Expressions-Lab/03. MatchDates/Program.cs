8926c8ef4871c2217d038142c32644d7bb87c7c0
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-04-29 10:53:09 +0300

    Add files via upload

---
+++ 02. Regular Expressions-Lab/03. MatchDates/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+
+string pattern = @"(?<Day>\d{2})(?<separator>[.\-/])(?<Month>[A-Z][a-z]{2})\<separator>(?<Year>\d{4})";
+
+string input = Console.ReadLine();
+
+Regex regex = new Regex(pattern);
+
+// .IsMatch връща стойност true, ако има съвпадения в текста (Matches)
+if (regex.IsMatch(input))
+{
+    MatchCollection dates = Regex.Matches(input, pattern);
+
+    foreach (Match match in dates)
+    {
+        string day = match.Groups["Day"].Value;
+        string month = match.Groups["Month"].Value;
+        string year = match.Groups["Year"].Value;
+
+        Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+    }
+}
