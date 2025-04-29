38d38e22c1bf335ed749ab1ddab80de407d53689
Teodor Kostov <148477548+rebelwithoutacause@users.noreply.github.com>
2025-04-29 10:52:22 +0300

    Delete Regular Expressions-Lab directory

--- Regular Expressions-Lab/01. MatchFullName/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Text.RegularExpressions;
-
-string pattern = @"\b[A-Z][a-z]+ [A-Z][a-z]+";
-
-string input = Console.ReadLine();
-
-MatchCollection regexMatches = Regex.Matches(input, pattern);
-
-Console.WriteLine(string.Join(" ", regexMatches));
